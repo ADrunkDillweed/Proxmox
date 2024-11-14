531913340302a07e1bef7c91c9f163547fc4b061
ADrunkDillweed <136045840+ADrunkDillweed@users.noreply.github.com>
2024-11-13 21:56:02 -0500

    Update debian-vm.sh

--- vm/debian-vm.sh
+++ vm/debian-vm.sh
@@ -385,6 +385,7 @@ nfs | dir)
   DISK_EXT=".qcow2"
   DISK_REF="$VMID/"
   DISK_IMPORT="-format qcow2"
+  DISK_SIZE="$80"
   THIN=""
   ;;
 btrfs)
@@ -408,7 +409,7 @@ pvesm alloc $STORAGE $VMID $DISK0 4M 1>&/dev/null
 qm importdisk $VMID ${FILE} $STORAGE ${DISK_IMPORT:-} 1>&/dev/null
 qm set $VMID \
   -efidisk0 ${DISK0_REF}${FORMAT} \
-  -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=2G \
+  -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=80G \
   -boot order=scsi0 \
   -serial0 socket \
   -description "<div align='center'><a href='https://Helper-Scripts.com'><img src='https://raw.githubusercontent.com/tteck/Proxmox/main/misc/images/logo-81x112.png'/></a>
